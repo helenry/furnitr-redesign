0aace7639ee35d8368700a6103cb7f43953db440
helenry <67642100+helenry@users.noreply.github.com>
2022-10-24 07:32:02 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,17 @@ navMobile.style.display = "none";
 
 bar.addEventListener("click", function () {
   navMobile.style.display = "";
+  document.body.style.overflow = "hidden";
 });
 
 close.addEventListener("click", () => {
   navMobile.style.display = "none";
+  document.body.style.overflow = "";
 });
 
 for (let i = 0; i < menus.length; i++) {
   menus[i].addEventListener("click", () => {
     navMobile.style.display = "none";
+    document.body.style.overflow = "";
   });
 }
